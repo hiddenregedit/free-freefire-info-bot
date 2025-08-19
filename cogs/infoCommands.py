d0d0aed1476a5587b7e3344bbde7ebcb33b0f964
hiddenregedit <hiddenregedit9@gmail.com>
2025-08-19 15:43:25 +0600

    Update infoCommands.py

--- cogs/infoCommands.py
+++ cogs/infoCommands.py
@@ -17,7 +17,7 @@ CONFIG_FILE = "info_channels.json"
 class InfoCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.api_url = "https://rawthug.onrender.com/info"
+        self.api_url = "http://raw.thug4ff.com/info?uid=945887015"
         self.generate_url = "https://generatethug.onrender.com/profile?uid={uid}"
         self.session = aiohttp.ClientSession()
         self.config_data = self.load_config()
