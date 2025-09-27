e3fda76301fdad5fb23b62bd4ff672217d6bf983
hiddenregedit <hiddenregedit9@gmail.com>
2025-09-27 11:31:14 +0600

    Update infoCommands.py

--- cogs/infoCommands.py
+++ cogs/infoCommands.py
@@ -17,6 +17,7 @@ class InfoCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.api_url = "http://raw.thug4ff.com/info"
+        self.card_url = "https://profile2.thug4ff.com/api/profile_card"
         self.generate_url = "https://profile2.thug4ff.com/api/profile"
         self.session = aiohttp.ClientSession()
         self.config_data = self.load_config()
@@ -245,7 +246,7 @@ class InfoCommands(commands.Cog):
                 embed.add_field(name="", value="\n".join(guild_info), inline=False)
 
             # profile card শেষে
-            embed.set_image(url=f"https://profile2.thug4ff.com/api/profile_card?uid={uid}")
+            embed.set_image(url=f"{self.card_url}?uid={uid}")
             embed.set_footer(text="DEVELOPED BY TANVIR")
             await ctx.send(embed=embed)
 
