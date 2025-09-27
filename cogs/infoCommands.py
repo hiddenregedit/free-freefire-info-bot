647422ec27c0eb80c3d34f1d6f84f1fd68512415
hiddenregedit <hiddenregedit9@gmail.com>
2025-09-27 12:09:08 +0600

    Update infoCommands.py

--- cogs/infoCommands.py
+++ cogs/infoCommands.py
@@ -239,8 +239,6 @@ class InfoCommands(commands.Cog):
                     ])
                 embed.add_field(name="", value="\n".join(guild_info), inline=False)
 
-            # profile card inside embed
-            embed.set_image(url=f"https://profile2.thug4ff.com/api/profile_card?uid={uid}")
             embed.set_footer(text="DEVELOPED BY TANVIR")
             await ctx.send(embed=embed)
 
