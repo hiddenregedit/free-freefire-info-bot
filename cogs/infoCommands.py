e04a7ec4dd7ec9d8cf1011da2183258fb583d7c7
hiddenregedit <hiddenregedit9@gmail.com>
2025-09-29 11:24:55 +0600

    Update infoCommands.py

--- cogs/infoCommands.py
+++ cogs/infoCommands.py
@@ -237,7 +237,6 @@ class InfoCommands(commands.Cog):
                         f"    **└─ CS Rank**: {'' if captain_info.get('showCsRank') else 'Not found'} {captain_info.get('csRankingPoints', 'Not found')} "
                     ])
            
-            embed.set_image(url=f"https://profile2.thug4ff.com/api/profile_card?uid={uid}")
             embed.set_footer(text="DEVELOPED BY TANVIR")
             embed.description = (embed.description or "") + "\n🔗 JOIN : https://discord.gg/RXSh8MpsZA"
             await ctx.send(embed=embed)
