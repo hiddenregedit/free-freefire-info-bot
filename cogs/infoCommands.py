bd85ceb96a1e7ac63da65853dd792a37b1068b12
hiddenregedit <hiddenregedit9@gmail.com>
2025-10-17 00:54:18 +0600

    Update infoCommands.py

--- cogs/infoCommands.py
+++ cogs/infoCommands.py
@@ -16,7 +16,7 @@ CONFIG_FILE = "info_channels.json"
 class InfoCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.api_url = "https://toxicinfo.vercel.app/info"
+        self.api_url = "http://raw.thug4ff.com/info"
         self.generate_url = "https://profile.thug4ff.com/api/profile"
         self.session = aiohttp.ClientSession()
         self.config_data = self.load_config()
